30d63b554dbd3a20f191683a6968be19ce758211
DB5640 <david.b5640@gmail.com>
2024-08-08 17:53:39 -0500

    QUeda a medias el de fibonacci

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const onlyLetterWord = word.replace(/\W/gi,"").toLowerCase()
+    isPalindrome = true
+    for (let index = 0; index < onlyLetterWord.length; index++) {
+        const element = onlyLetterWord.charAt(index);
+        if (element != onlyLetterWord.charAt(onlyLetterWord.length - index -1)) //Comparamos que cada letra sea igual a la longitud del onlyLetterWord de word menos el indice actual
+        { isPalindrome = false; break}
+    }
+    return isPalindrome
 };
+palindromes('A car, a man, a maraca.');
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (member) {
+    const fibo = [1, 1]
+    for (let i = 1; i <= member; i++) {
+        let lastNums =[fibo[i - 1],(fibo[i])]
+        fibo.push(lastNums.reduce((total, actual) => total + actual))
+    }
+    return fibo[fibo.length - 3]
 };
+
+let resultado = fibonacci(4)
 
 // Do not edit below this line
 module.exports = fibonacci;
